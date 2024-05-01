665f99e2eaa5c996929b1b6289ad74ef62518dc0
RoboticDragon <hlutz@brynmawr.edu>
2024-05-01 12:21:04 -0400

    Create f_stat.c
    This is only the function. This cannot run on its own.

---
+++ f_stat.c
@@ -0,0 +1,17 @@
+int f_stat(file_handle *stream, file_header *stat_buffer){
+    stat_buffer->first_FAT_idx = stream->first_FAT_idx;
+    for(int i = 0; i < sizeof(stream->name); i++){
+        stat_buffer->name[i] = stream->name[i];
+    }
+    stat_buffer->size = stream->size;
+    
+    fseek(disk, find_offset(stream->first_FAT_idx), SEEK_SET);
+    file_header* temp = malloc(sizeof(struct file_header));
+    fread(temp, sizeof(struct file_header), 1, disk);
+    stat_buffer->is_directory = temp->is_directory;
+    fread(stat_buffer->data_in_first_block, 496, 1, disk);
+    free(temp);
+    //retrieve information about a file
+    //updates the stat_buffer struct
+
+}
